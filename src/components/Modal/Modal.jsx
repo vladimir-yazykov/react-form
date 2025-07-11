d9b61eaf3f0bdb19ffd385401b09be72607ed05a
Vladimir Yazykov <neizerth@gmail.com>
2025-07-11 13:26:02 +0300

    feat: modal text

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,6 +1,6 @@
 import S from "./Modal.module.css";
 
-export function Modal({ show, children }) {
+export function Modal({ show, title, children }) {
   if (!show) {
     return;
   }
@@ -8,6 +8,7 @@ export function Modal({ show, children }) {
     <div className={S.container}>
       <div className={S.body}>
         <div className={S.close}>&times;</div>
+        <h2 className={S.title}>{title}</h2>
         {children}
       </div>
     </div>
